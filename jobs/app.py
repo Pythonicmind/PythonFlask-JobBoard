8eabd78377b1c2d70d9076a771ebd5668f3fdabd
atulchauhan89 <atul6101989@gmail.com>
2018-11-19 11:54:00 +0100

    Module 3 Checkin
    Module 3 checkin

--- jobs/app.py
+++ jobs/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, g
-import sqlite3 as sq
+import sqlite3
 
-PATH     = "/db/jobs.sqlite"
+PATH = "/db/jobs.sqlite"
 
 app = Flask(__name__)
 
 def open_connection():
     connection=getattr(g, '_connection', None)
     if connection is None:
-        connection = g._connection = sq.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sq.Row
     return connection
 def execute_sql(sql, values=(), commit=False, single=False):
